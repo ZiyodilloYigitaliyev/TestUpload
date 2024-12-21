e4ea47c73445f4047b6025dccfc2cba66188c664
adm1n_217 <142495809+adm1ngit@users.noreply.github.com>
2024-12-21 21:38:32 +0500

    finish

--- main.py
+++ main.py
@@ -35,7 +35,6 @@ class Question(Base):
 
 Base.metadata.create_all(bind=engine)
 
-# AWS konfiguratsiyasi
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_REGION_NAME = os.getenv("AWS_REGION_NAME")
