454000918832a82eb8e60f8282b35491272af0ce
Ziyodillo Yigitaliyev <113228133+ZiyodilloYigitaliyev@users.noreply.github.com>
2024-12-23 15:25:27 -0800

    👍

--- main.py
+++ main.py
@@ -18,9 +18,13 @@ from datetime import datetime, timedelta
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-Base = declarative_base()
+
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+    
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
 
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
