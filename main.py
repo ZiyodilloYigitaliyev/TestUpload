2ca9b4ea552d2546eefcc72cee5c0457c455099c
adm1n_217 <142495809+adm1ngit@users.noreply.github.com>
2024-12-25 02:22:42 +0500

    Update main.py

--- main.py
+++ main.py
@@ -60,22 +60,22 @@ class User(Base):
 Base.metadata.create_all(bind=engine)
 
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-AWS_REGION_NAME = os.getenv("AWS_REGION_NAME")
-BUCKET_NAME = os.getenv("BUCKET_NAME")
+AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
+S3_REGION = os.getenv("S3_REGION")
+S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
 
 # S3 mijozini sozlash
 s3_client = boto3.client(
     "s3",
     aws_access_key_id=AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-    region_name=AWS_REGION_NAME,
+    aws_secret_access_key=AWS_SECRET_KEY,
+    region_name=S3_REGION,
 )
 
 def upload_to_s3(file_path: str, key: str) -> str:
     try:
-        s3_client.upload_file(file_path, BUCKET_NAME, key)
-        s3_url = f"https://{BUCKET_NAME}.s3.{AWS_REGION_NAME}.amazonaws.com/{key}"
+        s3_client.upload_file(file_path, S3_BUCKET_NAME, key)
+        s3_url = f"https://{S3_BUCKET_NAME}.s3.{S3_REGION}.amazonaws.com/{key}"
         return s3_url
     except NoCredentialsError:
         raise HTTPException(status_code=500, detail="AWS credentials topilmadi.")
