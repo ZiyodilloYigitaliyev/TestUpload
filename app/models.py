c92a057eabf95eb7aec204bf16e046999bd93dc3
Ziyodillo Yigitaliyev <113228133+ZiyodilloYigitaliyev@users.noreply.github.com>
2024-12-23 23:20:36 -0800

    👍

--- app/models.py
+++ app/models.py
@@ -8,6 +8,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, nullable=False)
     hashed_password = Column(String, nullable=False)
+    tokens = relationship("Token", back_populates="user")
 
 class Token(Base):
     __tablename__ = "tokens"
