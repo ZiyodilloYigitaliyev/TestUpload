6922a14d070fa65e687a068fac534db289321e3b
O'tkir <142495809+adm1ngit@users.noreply.github.com>
2024-12-24 20:51:02 +0500

    Update database.py

--- app/database.py
+++ app/database.py
@@ -11,7 +11,7 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-if DATABASE_URL and DATABASE_URL.startwith("postgres://"):
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
   DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(DATABASE_URL)
