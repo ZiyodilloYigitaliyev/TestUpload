59d82f524ad045106318ae1d183dcd0d9a9bda39
Ziyodillo Yigitaliyev <113228133+ZiyodilloYigitaliyev@users.noreply.github.com>
2025-01-04 00:41:50 -0800

    *_*

--- app/main.py
+++ app/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 from app.routes import user_routes, file_routes
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Barcha domenlarga ruxsat
+    allow_credentials=True,
+    allow_methods=["*"],  # Barcha metodlarga ruxsat
+    allow_headers=["*"],  # Barcha so'rov sarlavhalariga ruxsat
+)
 
 app.include_router(user_routes.router)
 app.include_router(file_routes.router)
