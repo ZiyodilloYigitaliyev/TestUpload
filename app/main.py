be1b056ca5f77000917b8da648817ca5bf905775
Ziyodillo Yigitaliyev <113228133+ZiyodilloYigitaliyev@users.noreply.github.com>
2025-01-06 23:01:36 -0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 from app.routes import user_routes, file_routes
 from fastapi.middleware.cors import CORSMiddleware
-app = FastAPI(
+app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Barcha domenlarga ruxsat
